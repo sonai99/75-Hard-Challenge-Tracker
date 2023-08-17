037f5db12253f5cb10ed8367ebb5e30acf91257c
sonai99 <anirbandutta428@gmail.com>
2023-08-17 19:51:44 +0530

    added about section

--- client/src/Components/LandingPage/LandingPage.js
+++ client/src/Components/LandingPage/LandingPage.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./Header/Header";
 import Rules from "./Rules/Rules";
+import About from "./About/About";
 
 const LandingPage = () => {
   return (
     <div>
       <Header />
+      <About />
       <Rules />
     </div>
   );
